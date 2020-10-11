cfa99d4d5d83a38095395a22ca5cafe7916ccbf8
liuqiao <949396818@qq.com>
2020-10-11 09:33:16 +0800

    index 退出功能

--- assets/js/baseapi.js
+++ assets/js/baseapi.js
@@ -1,5 +1,9 @@
 // 因为每次通过jquery运行 ajax的get/post请求时，都会优先执行ajaxPrefilter这个函数，这个函数会获取，ajax的相关配置对象，档次函数调用完毕后，才会执行真正的get/post请求
 $.ajaxPrefilter(function(options){
-    options.url = 'http://ajax.frontend.itheima.net'+options.url
-console.log(options.url);
+    options.url = 'http://ajax.frontend.itheima.net'+options.url 
+   if(options.url.indexOf('/my/') !== -1){
+       options.header={
+        Authorization:localStorage.getItem('token')||""
+       }
+   }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+$(function () {
+    // 调用ajax，请求数据
+    userdata()
+    // 实现退出功能
+
+    $('#out').on('click', function () {
+        console.log("ok");
+        layer.confirm('是否退出', { icon: 3, title: '提示' }, function (index) {
+            // 点击退出
+            localStorage.removeItem('token')
+            location.href = "../login.html"
+            layer.close(index);
+        });
+    })
+})
+//  渲染函数
+function userdata() {
+    $.ajax({
+        type: "GET",
+        url: "/my/userinfo",
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用化信息失败')
+            }
+            touxiang(res.data)
+        }
+    });
+}
+// 判断头像
+function touxiang(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html(`欢迎${name}`)
+    if (user.user_pic !== null) {
+        $('#welcome').hide()
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('#welcome').html(first).show()
+    }
+}
